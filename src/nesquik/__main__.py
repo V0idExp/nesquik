6a0e9799d0eda5aa43aba0d15b00e269e8f4f563
Ivan Nikolaev <voidexp@gmail.com>
2021-03-03 11:21:54 +0300

    CLI: allow to specify org address

--- src/nesquik/__main__.py
+++ src/nesquik/__main__.py
@@ -6,20 +6,39 @@ from lark import logger
 from nesquik.compiler import compile
 from nesquik.parser import parse
 
+
 logger.setLevel(logging.DEBUG)
+
+
+class AddrType(click.ParamType):
+
+    name = "address"
+
+    def convert(self, value, param, ctx):
+        try:
+            if value[:2].lower() == '0x':
+                return int(value[2:], base=16)
+            return int(value, base=10)
+        except (TypeError, ValueError):
+            self.fail(
+                f'expected an address in decimal or hexadecimal format, '
+                f'got {value}')
 
 
 @click.command()
 @click.argument('file', type=click.File('r'))
-@click.option('-o', type=click.File('wb'), required=False)
-def nq(file, o):
+@click.option('-o', '--out', type=click.File('wb'), required=False,
+    help='Output binary file name')
+@click.option('--org', type=AddrType(), required=False, default=0xc000,
+    help='Program start address')
+def nq(file, out, org):
     code = file.read()
     print(code)
 
     ast = parse(code)
     print(ast.pretty())
 
-    program = compile(ast)
+    program = compile(ast, org)
     print('\n'.join(program.asm))
 
     print()
@@ -29,8 +48,9 @@ def nq(file, o):
     print()
     print(f'size: {len(program.obj)} bytes')
 
-    if o:
-        o.write(program.obj)
+    if out:
+        out.write(program.obj)
+
 
 if __name__ == '__main__':
     nq()  # pylint: disable=no-value-for-parameter
