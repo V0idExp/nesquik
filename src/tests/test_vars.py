a3c42edb5d89df3a38b89ab468c7eaaaa1252fbd
Ivan Nikolaev <voidexp@gmail.com>
2021-03-03 20:30:16 +0300

    More tests for variables

--- src/tests/test_vars.py
+++ src/tests/test_vars.py
@@ -1,28 +1,57 @@
 import pytest
-
+from nesquik.compiler import NESQuikUndefinedVariable
 
 @pytest.mark.parametrize('code, exp_result', [
     (
-        'a = 2 + 3\n'
-        'b = 4 + 5\n'
-        'return a + b',
+        '''
+        a = 2 + 3
+        b = 4 + 5
+        return a + b
+        ''',
         14,
     ),
     (
-        'a = 2 + 2\n'
-        'b = 6 - a\n'
-        'c = a * b\n'
-        'return c * 2 - b',
+        '''
+        a = 2 + 2
+        b = 6 - a
+        c = a * b
+        return c * 2 - b
+        ''',
         14,
     ),
     (
-        'a = 5\n'
-        'b = 23 / a\n'
-        'c = 14 / 15\n'
-        'return a * b - -(a / 5)',
+        '''
+        a = 5
+        b = 23 / a
+        c = 14 / 15
+        return a * b - -(a / 5)
+        ''',
         21,
-    )
+    ),
+    (
+        '''
+        a = 5
+        b = a * 5
+        a = b
+        return a
+        ''',
+        25,
+    ),
 ])
 def test_vars(cpu, code, exp_result):
     cpu.compile_and_run(code)
     assert cpu.a == exp_result
+
+
+@pytest.mark.parametrize('code', [
+    '''
+    a = 5
+    b = a + c
+    ''',
+    '''
+    c = d
+    ''',
+])
+def test_undefined_vars(cpu, code):
+    with pytest.raises(NESQuikUndefinedVariable):
+        cpu.compile_and_run(code)
